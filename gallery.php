d4a13bce1c02d42885046b2588626dc4bb18f48b
Bilawal Aziz <bilawalaziz59@gmail.com>
2024-08-13 11:20:41 +0500

    first commit

---
+++ gallery.php
@@ -0,0 +1,66 @@
+<?php
+$page = '1212 MED | gallery';
+$pageDesc = '""';
+$pageTag = '""';
+include 'includes/header.php';
+$page = 'gallery';
+?>
+
+
+<section class="main1" data-aos="fade-down-left" data-aos-duration="1500">
+    <div class="container">
+        <div class="main2">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div class="main3">
+                        <h2>Our <span class="home12">Gallery</span></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="main4">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="main5">
+                    <img src="images/pic22.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                    <img src="images/pic23.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                    <img src="images/pic24.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                    <img src="images/pic25.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="main5">
+                    <img src="images/pic26.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                    <img src="images/pic27.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                    <img src="images/pic28.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                    <img src="images/pic29.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="main5">
+                    <img src="images/pic30.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                    <img src="images/pic31.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                    <img src="images/pic32.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                    <img src="images/pic33.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-duration="1500">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<a id="button_top"><i class="fa fa-chevron-up"></i></a>
+
+<?php
+include 'includes/footer.php';
+$page = 'gallery';
+?>
---
+++ term.php
@@ -0,0 +1,51 @@
+<?php
+$page = '1212 MED | term';
+$pageDesc = '""';
+$pageTag = '""';
+include 'includes/header.php';
+$page = 'term';
+?>
+
+
+
+<section class="main6" data-aos="fade-down-left" data-aos-duration="1500">
+    <div class="container">
+        <div class="main-7">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div class="main7">
+                        <h2>TERMS & <span class="home12">CONDITIONS</span></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="main8" data-aos="fade-right" data-aos-duration="1500">
+    <div class="container">
+        <div class="row main-9">
+            <div class="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 col-xxl-9">
+                <div class="main9">
+                    <h2>Privacy Policy:</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                </div>
+
+                <div class="main9">
+                    <h2>Terms & Condition</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<a id="button_top"><i class="fa fa-chevron-up"></i></a>
+
+<?php
+include 'includes/footer.php';
+$page = 'term';
+?>
